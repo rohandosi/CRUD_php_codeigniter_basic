b5f60373c525552e53bf6e6aa7be51b26ecf25ec
Rohan Dosi <rohandosi7@gmail.com>
2017-06-15 10:32:02 +0530

    header and footer added

---
+++ application/views/templates/footer.php
@@ -0,0 +1,4 @@
+<p> Playing with Codeigniter. Have Fun! Cheers! </p>
+</body>
+</html>
+
---
+++ application/views/templates/header.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>CRUD Codeigniter</title>
+</head>
+<body>
+<h1>Basic CRUD</h1>
+<p><a href="<?php echo site_url('news');  ?>">Home</a><a href="<?php echo site_url('news/create'); ?>">Add</a></p>
+
